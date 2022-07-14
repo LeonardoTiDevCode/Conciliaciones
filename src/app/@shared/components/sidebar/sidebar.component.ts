94c263e159e8664e93fc5f07dbfbf994e118a206
unknown <juarez_s3@hotmail.com>
2022-07-13 23:27:16 -0500

    New styles sidebar

--- src/app/@shared/components/sidebar/sidebar.component.ts
+++ src/app/@shared/components/sidebar/sidebar.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  logoTotalPanel = '../assets/images/Logo-Totalplay.svg';
 
   constructor() { }
 
   ngOnInit(): void {
+    const selected = document.querySelector(".selected");
+    const optionsContainer = document.querySelector(".options-container");
+
+    const optionsList = document.querySelectorAll(".option");
+
+    selected.addEventListener("click", () => {
+      optionsContainer && optionsContainer.classList.toggle("active");
+    });
+
+    optionsList.forEach((o) => {
+      o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer && optionsContainer.classList.remove("active");
+      });
+    });
   }
 
 }
