6ab4d1f544d51313de119840930399b5e167e6ff
unknown <juarez_s3@hotmail.com>
2022-08-01 22:06:35 -0500

    :sparkles: Modal Comparaciones

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   logoTotal = '../assets/images/Grupo_3.png';
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  goHome() {
+    this.router.navigateByUrl('/panel');
   }
 
 }
